fdd222b2e29c96b4bafced7068d83d147f5d7bd4
Katherine Wyers <katherine@katherinewyers.com>
2026-06-15 13:54:20 +0100

    Initial Commit

---
+++ src/test/java/com/katherinewyers/vsc/secure_system/SecureSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katherinewyers.vsc.secure_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
