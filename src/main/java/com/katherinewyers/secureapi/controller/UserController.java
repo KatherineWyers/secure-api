0baa168f22c7eab9b010017009595757c90202b2
Katherine Wyers <katherine@katherinewyers.com>
2026-06-19 16:20:57 +0100

    feat: Generate JWT token on successful login

--- src/main/java/com/katherinewyers/secureapi/controller/UserController.java
+++ src/main/java/com/katherinewyers/secureapi/controller/UserController.java
@@ -3,6 +3,7 @@ package com.katherinewyers.secureapi.controller;
 import com.katherinewyers.secureapi.model.User;
 import com.katherinewyers.secureapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,11 @@ public class UserController {
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return service.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return service.verify(user);
     }
 
 }
--- src/main/java/com/katherinewyers/secureapi/service/UserService.java
+++ src/main/java/com/katherinewyers/secureapi/service/UserService.java
@@ -2,19 +2,40 @@ package com.katherinewyers.secureapi.service;
 
 import com.katherinewyers.secureapi.model.User;
 import com.katherinewyers.secureapi.repository.UserRepository;
+import io.jsonwebtoken.Jwt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
     private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
 
     public User register(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public String verify(User user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUsername());
+        }
+
+        return "Failed to log in";
     }
 }
